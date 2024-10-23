7e2587770481754345792373dbbbfe92c23382f2
Elias Ortega <elias.ortega@ufl.edu>
2024-10-23 17:59:16 -0400

    added decode functionality

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -9,8 +9,9 @@ def encode(number_string):
             password_out = password_out + str(int(char) - 7) # create a way to handle cases where the number wraps around
     return password_out
 
-def decode(): # the decode function goes here
-    pass
+def decode(encoded):
+    encoded = int(str(encoded).replace("0","a").replace("1","b").replace("2","c").replace("3","d").replace("4","e").replace("5","f").replace("6","g").replace("7","h").replace("8","i").replace("9","j").replace("a","7").replace("b","8").replace("c","9").replace("d","0").replace("e","1").replace("f","2").replace("g","3").replace("h","4").replace("i","5").replace("j","6"))
+    return encoded
 
 def main():
     menu_continue = True
@@ -21,7 +22,8 @@ def main():
           password = str(input("Please enter your password to encode:")) # take input from the user
           encoded_password = encode(password)
       elif choice == "2":
-          pass # decoder goes here
+          decoded = decode(encoded_password)
+          print(f"The encoded password is {encoded_password}, and the original password is {decoded}.\n")
       elif choice == "3":
           menu_continue = False
       else:
